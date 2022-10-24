06a35f01a6e000763eacbf681818b758d41c1171
lzyjin <1247y.j@gmail.com>
2022-10-24 22:22:26 +0900

    컴포넌트 디자인 패턴 - Common

---
+++ design/common/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <app-header :title="appTitle"></app-header>
+    <app-content :items="items" @renew="renewItems"></app-content>
+  </div>
+</template>
+
+<script>
+
+import AppHeader from "@/components/AppHeader";
+import AppContent from "@/components/AppContent";
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppContent,
+  },
+  data() {
+    return {
+      appTitle: 'Common Approch',
+      items: [10, 20, 30],
+    }
+  },
+  methods: {
+    renewItems() {
+      this.items = [40, 50, 60];
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ design/common/src/components/AppContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="$emit('renew')">renew items</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppContent",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
