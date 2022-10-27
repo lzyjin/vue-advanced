b517f3a1aef69c1b61bb8661371a8568a57a2f97
lzyjin <1247y.j@gmail.com>
2022-10-27 23:07:58 +0900

    컴포넌트 디자인 패턴 - Controlled

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<!--  <check-box :checked="checked"></check-box>-->
+
+  <!-- 에러 해결방법: props 대신 v-model을 사용해 데이터를 내려보자 -->
+  <!-- slot과 마찬가지로 하위컴포넌트의 input의 데이터가 상위컴포넌트에서 관리된다 (장점) -->
+  <!-- 작은 단위로 쪼개는 컴포넌트까지 관리할 수 있다. -->
+  <check-box v-model="checked"></check-box>
+</template>
+
+<script>
+
+import CheckBox from "./components/CheckBox";
+
+export default {
+  name: 'App',
+  components: {
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: false,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+<!--  <input type="checkbox" v-model="checked">-->
+  <!-- 화면에서 체크박스를 클릭하면 콘솔에 에러가 뜬다 -->
+  <!--
+    [Vue warn]: Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders.
+    Instead, use a data or computed property based on the prop's value. Prop being mutated: "checked"
+  -->
+
+  <input type="checkbox" :value="value" @click="toggleCheckBox">
+</template>
+
+<script>
+export default {
+  name: "CheckBox",
+  // props: ['checked'],
+
+  // input은
+  // @input 이벤트
+  // :value 값
+
+  prop: ['value'],
+  methods: {
+    toggleCheckBox() {
+      this.$emit('input', !this.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
